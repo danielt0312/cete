bf70888b3ec723c308f091995ad58d163ec0e7b9
Daniel Torres <2130072@upv.edu.mx>
2024-01-31 02:24:03 -0600

    etapas-alert

--- app/Http/Controllers/EtapasController.php
+++ app/Http/Controllers/EtapasController.php
@@ -13,15 +13,36 @@ class EtapasController extends Controller
         foreach (CatEtapa::all() as $index => $value) {
             $attributes = $value->getAttributes();
             $attributes['editar'] = '
-                <a class="btn btn-primary btn-editar" href="/etapas/grabar/'.$attributes['id'].'">
-                    <i class="fas fa-pencil-alt"></i>
-                </a>
+                <div class="dropdown">
+                    <a class="btn" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                        </svg>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="'.route('agregar_etapa', ['id' => $attributes['id']]).'">Editar</a></li>
+                        <li><a class="dropdown-item" href="'.route('eliminar_etapa', ['id' => $attributes['id']]).'">Eliminar</a></li>
+                    </ul>
+                </div>
             ';
 
             $etapas[$index] = $attributes;
         }
 
         return(view('etapas.index', ['etapas' => $etapas]));
+    }
+
+    public function delete()
+    {
+        $etapa = CatEtapa::find(request('id'));
+
+        if ($etapa) {
+            $etapa->delete();
+
+            return redirect()->route('index_etapas')->with('success', 'Registro eliminado correctamente');
+        } else {
+            return redirect()->route('index_etapas')->with('error', 'Registro no encontrado');
+        }
     }
 
     public function store()
